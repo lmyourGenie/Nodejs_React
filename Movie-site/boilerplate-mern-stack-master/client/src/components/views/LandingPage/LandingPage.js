b9874c2d5af1603d7373d6cb505d241c00b3d991
leyujin1007 <leyujin1007@naver.com>
2021-02-27 23:02:48 +0900

    GridCard 생성, LoadMore function 구현

--- Movie-site/boilerplate-mern-stack-master/client/src/components/views/LandingPage/LandingPage.js
+++ Movie-site/boilerplate-mern-stack-master/client/src/components/views/LandingPage/LandingPage.js
@@ -1,23 +1,44 @@
 import React, { useEffect, useState } from 'react'
-import { FaCode } from "react-icons/fa";
+import { FaCode, FaSortAmountDown } from "react-icons/fa";
 import { API_KEY, API_URL, IMAGE_BASE_URL } from '../../Config';
 import MainImage from './Sections/MainImage';
+import GridCards from '../commons/GridCards';
+import { Row } from 'antd';
 
 function LandingPage() {
 
     const [Movies, setMovies] = useState([])
     const [MainMovieImage, setMainMovieImage] = useState(null)
+    const [CurrentPage, setCurrentPage] = useState(0)
 
     useEffect(() => {
         const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+        //페이지가 1
+        //즉, 처음 20개의 영화를 받아서 카드에 담아 보여줌
+        fetchMovies(endpoint)
+        //앱이 로드되자마자 실행
 
+    } , [])
+
+
+    const fetchMovies = ( endpoint ) => {
         fetch(endpoint)
         .then(response => response.json())
         .then(response => {
-            setMovies([response.results])
+            setMovies([...Movies, ...response.results])
+            //setMovies([...response.results])
+            //이렇게하면 덮어씌우기 때문에 LoadMore을 누르면 이전 영화 20개가 사라짐
             setMainMovieImage(response.results[0]) //첫 번째 영화가 메인이미지로
+            setCurrentPage(response.page)
         })
-    } , [])
+    }
+
+
+    const loadMoreItems = () => {
+        const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${CurrentPage + 1}`;
+        //loadMore을 누를수록 page 숫자가 올라가므로 동적으로 할당
+        fetchMovies(endpoint)
+    }
 
 
 
@@ -39,10 +60,26 @@ function LandingPage() {
                 <hr />
 
                 {/* Movie Grid Card */}
+                <Row gutter={[16, 16]}>              
+                    {Movies && Movies.map((movie, index) => (
+                    /*key를 넣어야 에러가 안남*/
+                        <React.Fragment key={index}>
+
+                            <GridCards 
+                                image={movie.poster_path ?
+                                    `${IMAGE_BASE_URL}w500${movie.poster_path}` : null}
+
+                                movieId={movie.id}
+                                movieName={movie.original_title}
+                            />
+                        </React.Fragment>
+                    ))}
+
+                </Row>
 
             </div>
             <div style={{ display: 'flex', justifyContent: 'center' }}>
-                    <button> Load More </button>
+                    <button onClick={loadMoreItems}> Load More </button>
             </div>
         </div>
     )
---
+++ Movie-site/boilerplate-mern-stack-master/client/src/components/views/commons/GridCards.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Col } from 'antd';
+
+function GridCards(props) {
+    return (
+        //한 컬럼에 24사이즈
+        //props으로 받아온 정보들을 그리드카드에 넣어줌
+        <Col lg={6} md={8} xs={24}>
+            <div style={{ position: 'relative' }}>
+                <a href={`/movie/${props.movieId}`} >
+                    <img style={{ width:'100%', height:'320px' }} src={props.image} alt={props.movieName} />
+                </a>
+            </div>       
+        </Col>
+    )
+}
+
+export default GridCards
